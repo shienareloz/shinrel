c1db974f26ea2df7a9f99bd5fba895545bd1f0cd
Shiena Reloz <relozshiena2gmail.com>
2025-04-04 17:44:30 +0800

    modified roles table and added foreign key to users table

--- database/migrations/2025_03_28_100305_add_role_to_users_table.php
+++ database/migrations/2025_03_28_100305_add_role_to_users_table.php
@@ -4,25 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+return new class extends Migration{
+    public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->unsignedBigInteger('role_id')->after('password');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('role_id');
         });
     }
 };
